7c11b40e607e5fbdd24d8805bd24e3bb881429af
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-12-21 05:35:00 -0800

    added backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import './App.css';
+import TinderCards from "./Components/TinderCards/TinderCards";
+import SwipeButtons from "./Components/SwipeButtons/SwipeButtons";
+
 
 function App() {
   return (
     <div className='app'>
-      <Header/>
+       <Router>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <TinderCards/>
+            <SwipeButtons />
+          </Route>
+        </Switch>
+      </Router>
 
     </div>
   );
---
+++ src/Components/TinderCards/TinderCards.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
+
+
+
+
+function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name:'Whatever',
+            url: ''
+        },
+        {
+            name: 'whatever 2',
+            url: ""
+        },
+    ]);
+
+    return (
+        <div>
+            <div className="tinderCards__cardContainer" >
+                {people.map((person) =>(
+                    <TinderCard
+                    className="swipe"
+                    key={person.name}
+                    preventSwipe={["up", "down"]}>
+                        <div
+                        style={{ backgroundImage: `url(${person.url})`}}
+                        className="card"
+                        >
+                            <h3>{person.name}</h3>
+
+                        </div>
+                    
+                    </TinderCard>
+                ))}
+
+            </div>
+        </div>
+    );
+}
+
+export default TinderCards;
